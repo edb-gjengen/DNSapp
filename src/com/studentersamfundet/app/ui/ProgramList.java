269a468c307e17973627c6b57daad384369b798a
Robert Kolner <robert.kolner@gmail.com>
2011-09-14 18:07:36 +0200

    Cleaned up xml-files.

--- src/com/studentersamfundet/app/ui/ProgramList.java
+++ src/com/studentersamfundet/app/ui/ProgramList.java
@@ -4,6 +4,7 @@ import org.w3c.dom.NodeList;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.Toast;
@@ -25,8 +26,8 @@ public class ProgramList extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_list);
         
-        // View v = this.findViewById(R.id.program_button);
-        // v.setClickable(false);
+        View v = this.findViewById(R.id.program_button);
+        v.setClickable(false);
         
         // Do we have intarwebs?
         // YAY = Fetch the feed.
