27546d49035bd53d688ef5ad3c2556d2f0d8f4d8
Robert Kolner <robert.kolner@gmail.com>
2011-10-19 22:35:16 +0200

    Added Linkify to 'Bli medlem'

--- src/com/studentersamfundet/app/ui/JoinUsActivity.java
+++ src/com/studentersamfundet/app/ui/JoinUsActivity.java
@@ -1,6 +1,8 @@
 package com.studentersamfundet.app.ui;
 
 import android.os.Bundle;
+import android.text.util.Linkify;
+import android.widget.TextView;
 
 import com.studentersamfundet.app.R;
 
@@ -8,5 +10,8 @@ public class JoinUsActivity extends BaseDnsActivity {
 	   public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.join_us);
+	        
+	        TextView tv = (TextView)findViewById(R.id.join_us_textview);
+	        Linkify.addLinks(tv, Linkify.EMAIL_ADDRESSES);
 	    }
 }
