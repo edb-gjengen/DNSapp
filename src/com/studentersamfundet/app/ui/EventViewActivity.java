5fe1bd2d4116868e5582c549430cbbaba8bce21c
Lars Jansøn Engvik <lars@studentersamfundet.no>
2013-01-17 15:44:29 +0100

    Updated header and icon with the new logo.

--- src/com/studentersamfundet/app/ui/EventViewActivity.java
+++ src/com/studentersamfundet/app/ui/EventViewActivity.java
@@ -11,7 +11,6 @@ import android.text.util.Linkify;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
